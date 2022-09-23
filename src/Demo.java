54f2f4866ef96eb383901c1698b64a4f2cbea916
Craig <craig.woodcock89@hotmail.co.uk>
2022-09-23 18:51:07 +0100

    DEMO class complete working version as requested by Trevor

--- src/Demo.java
+++ src/Demo.java
@@ -1,36 +1,53 @@
+import java.util.Scanner;
+
 public class Demo {
     public static void main(String[] args) {
-        double a = 4;
-        double b = 6;
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("lets enter a couple of numbers:");
+        System.out.print("Enter the first number: ");
+        double inputtedNum1 = reader.nextDouble();
+        System.out.print("Enter the second number: ");
+        double inputtedNum2 = reader.nextDouble();
         double c = 0;
-        //this is the method call
-        //c = sumMethod(a,b);
-        //display(c);
-        //c = subtractMethod(a,b);
-        //display(c);
-        //c = multiplyMethod(a,b);
-        //display(c);
+
         displayMenu();
+
+        int answer = reader.nextInt();
+        if (answer==1){
+            c = sumMethod(inputtedNum1,inputtedNum2);
+            display(c);
+
+        } else if (answer==2) {
+            c = subtractMethod(inputtedNum1,inputtedNum2);
+            display(c);
+
+        } else if (answer==3) {
+            c = multiplyMethod(inputtedNum1,inputtedNum2);
+            display(c);
+        }else {
+            System.out.println("invalid option!");
+        }
 
     }
     public static void displayMenu(){
-        System.out.println("1. calc menu");
-        System.out.println("2. add numbers");
-        System.out.println("3. subtract numbers");
-        System.out.println("4. multiply numbers");
-        System.out.print("please choose an option!");
+        System.out.println("What would you like to do? 1, 2 or 3: ");
+        System.out.println("1. add numbers");
+        System.out.println("2. subtract numbers");
+        System.out.println("3. multiply numbers");
+
     }
-    public static double sumMethod(double n, double m){
-        return (n+m);
-    }
-    public static void display(double sum){
-        System.out.println("The Sum Is: "+sum);
-    }
-    public static double subtractMethod(double n, double m){
-        return(n-m);
-    }
-    public static double multiplyMethod(double n, double m){
-        return(n*m);
+    public static double sumMethod(double a, double b){
+        return (a+b);
     }
 
+    public static double subtractMethod(double a, double b){
+        return(a-b);
+    }
+    public static double multiplyMethod(double a, double b){
+        return(a*b);
+    }
+    public static void display(double sum){
+        System.out.println("The answer is: "+sum);
+    }
 }
